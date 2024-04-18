5ceb054b2fb9600a2aa71fb9a8a10f910a1ed0ee
Aftar Fadilah <aftar.fadilah@nosc.ai>
2024-04-18 08:18:06 +0700

    Add Bottom Navigation Bar

---
+++ app/src/main/java/com/aftarfadilah/a160421095hobbyapp/ui/activities/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.aftarfadilah.a160421095hobbyapp.ui.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.aftarfadilah.a160421095hobbyapp.R
+import com.aftarfadilah.a160421095hobbyapp.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+//        setContentView(R.layout.activity_main)
+
+        navController = (supportFragmentManager.findFragmentById(R.id.fragmentHost) as NavHostFragment).navController
+        NavigationUI.setupActionBarWithNavController(this, navController, binding.drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+        binding.bottomNav.setupWithNavController(navController)
+    }
+}
