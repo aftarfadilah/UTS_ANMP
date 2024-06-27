92344ade5a27a45367a1343a397ff73a8eb439e5
Aftar Fadilah <aftar.fadilah@nosc.ai>
2024-06-27 17:23:23 +0700

    Fix latest

--- app/src/main/java/com/aftarfadilah/a160421095hobbyapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/aftarfadilah/a160421095hobbyapp/ui/activities/MainActivity.kt
@@ -29,4 +29,5 @@ class MainActivity : AppCompatActivity() {
         NavigationUI.setupWithNavController(binding.navView, navController)
         binding.bottomNav.setupWithNavController(navController)
     }
+
 }
